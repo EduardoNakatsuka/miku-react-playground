156d586fc75d324fd4b9e0a1b83747e422bdfbdb
Eduardo Silva <edu_godoys@hotmail.com>
2021-03-24 13:46:58 -0300

    refactor(plugin): Refactor ifs into build.onResolve with filters

--- src/plugins/unpk-path-plugin.ts
+++ src/plugins/unpk-path-plugin.ts
@@ -14,25 +14,25 @@ export const unpkgPathPlugin = (inputCode: string) => {
         onResolve is called whenever the building process is trying to figure it out
           where the file is stored
       */
+
+      // Handle root entry file of 'index.js'
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { namespace: 'a', path: 'index.js' };
+      });
+
+      // Handle relative paths in a module
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: 'a',
+          path: new URL(
+            args.path,
+            'https://unpkg.com' + args.resolveDir + '/'
+          ).href,
+        };
+      });
+
+      // Handle main file of a module
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args);
-        if (args.path === 'index.js') {
-          return {
-            namespace: 'a',
-            path: args.path
-          };
-        }
-
-        if (args.path.includes('./') || args.path.includes('../')) {
-          return {
-            namespace: 'a',
-            path: new URL(
-              args.path,
-              'https://unpkg.com' + args.resolveDir + '/'
-            ).href,
-          };
-        }
-
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`,
