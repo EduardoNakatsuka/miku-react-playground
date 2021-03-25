52313d3b2026d7641c849536461fe031cc7ac0d0
Eduardo Silva <edu_godoys@hotmail.com>
2021-03-25 16:40:50 -0300

    feature(index): Now the iframe works and everything kind of works

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,14 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm'
+      wasmURL: '/esbuild.wasm' // wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm' Could also use this but
     });
   };
 
@@ -37,8 +38,30 @@ const App = () => {
       },
     });
 
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
     <div>
@@ -49,6 +72,8 @@ const App = () => {
       </div>
 
       <pre>{code}</pre>
+
+      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} />
     </div>
   )
 };
